223921ae8b355c92f7071f51fe73199f1e5df043
WIN-24V478DHT3N\student <2022java42-student006@la-study.com>
2022-05-17 10:12:31 +0900

    ショッピング例

---
+++ src/main/java/la/dao/OrderDAO.java
@@ -0,0 +1,125 @@
+package la.dao;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import la.bean.CartBean;
+import la.bean.CustomerBean;
+import la.bean.ItemBean;
+
+public class OrderDAO {
+
+	private String url = "jdbc:postgresql:sample";
+	private String user = "student";
+	private String pass = "himitu";
+	
+	public OrderDAO() throws DAOException{
+		try {
+			Class.forName("org.postgresql.Driver");
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+			throw new DAOException("JDBCドライバの登録に失敗しました。");
+		}
+	}
+	
+	public int saveOrder(CustomerBean customer, CartBean cart) throws DAOException{
+		
+		int customerNumber = 0;
+		String sql = "SELECT nextval('customer_code_seq')";
+		
+		try(
+			Connection con = DriverManager.getConnection(url, user, pass);
+			PreparedStatement st = con.prepareStatement(sql);
+			ResultSet rs = st.executeQuery();){
+			
+				if(rs.next()) {
+					customerNumber = rs.getInt(1);
+				}
+			
+			}catch(SQLException e) {
+				e.printStackTrace();
+				throw new DAOException("レコードの取得に失敗しました。");
+			}
+		
+		sql = "INSERT INTO customer VALUES(?, ?, ?, ?, ?)";
+		
+		try(
+			Connection con = DriverManager.getConnection(url, user, pass);
+			PreparedStatement st = con.prepareStatement(sql);){
+				
+				st.setInt(1, customerNumber);
+				st.setString(2, customer.getName());
+				st.setString(3, customer.getAddress());
+				st.setString(4, customer.getTel());
+				st.setString(5, customer.getEmail());
+				
+				st.executeUpdate();
+				
+			}catch(SQLException e) {
+				e.printStackTrace();
+				throw new DAOException("レコードの操作に失敗しました。");
+			}
+		
+		int orderNumber = 0;
+		sql = "SELECT nextval('ordered_code_seq')";
+		
+		try(
+			Connection con = DriverManager.getConnection(url, user, pass);
+			PreparedStatement st = con.prepareStatement(sql);
+			ResultSet rs = st.executeQuery();){
+			
+				if(rs.next()) {
+					orderNumber = rs.getInt(1);
+				}
+			
+			}catch(SQLException e) {
+				e.printStackTrace();
+				throw new DAOException("レコードの操作に失敗しました。");
+			}
+		
+		sql = "INSERT INTO ordered VALUES(?, ?, ?, ?)";
+		
+		try(
+			Connection con = DriverManager.getConnection(url, user, pass);
+			PreparedStatement st = con.prepareStatement(sql);){
+				
+				st.setInt(1, orderNumber);
+				st.setInt(2, customerNumber);
+				Date today = new Date(System.currentTimeMillis());
+				st.setDate(3, today);
+				st.setInt(4, cart.getTotal());
+				
+				st.executeUpdate();
+				
+			}catch(SQLException e) {
+				e.printStackTrace();
+				throw new DAOException("レコードの操作に失敗しました。");
+			}
+		
+		sql = "INSERT INTO ordered_detail VALUES(?, ?, ?)";
+		
+		try(
+			Connection con = DriverManager.getConnection(url, user, pass);
+			PreparedStatement st = con.prepareStatement(sql);){
+					
+			List<ItemBean> items = cart.getItems();
+			
+			for(ItemBean item :items) {
+				st.setInt(1, orderNumber);
+				st.setInt(2, item.getCode());
+				st.setInt(3, item.getQuantity());
+					
+				st.executeUpdate();
+			}
+			return orderNumber;
+		}catch(SQLException e) {
+			e.printStackTrace();
+			throw new DAOException("レコードの操作に失敗しました。");
+		}
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,4 @@
+
+		<jsp:forward page="/ShowItemServlet">
+			<jsp:param name="action" value="top" />
+		</jsp:forward>
