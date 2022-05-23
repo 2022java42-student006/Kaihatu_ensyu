d5be92f6cd732a3d708947f5831497502f7f6e40
WIN-24V478DHT3N\student <2022java42-student006@la-study.com>
2022-05-23 12:21:05 +0900

    新しいサーブレットファイルを追加しました。

--- src/main/java/la/servlet/MyPageServlet.java
+++ src/main/java/la/servlet/MyPageServlet.java
@@ -1,7 +1,6 @@
 package la.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,13 +9,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/MenuServlet")
-public class MenuServlet extends HttpServlet {
-   
+@WebServlet("/MyPageServlet")
+public class MyPageServlet extends HttpServlet {
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html;charset=UTF-8");
-		PrintWriter out = response.getWriter();
-		out.println("Hello World!!");
+		
 	}
 
 	
--- src/main/java/la/servlet/RegServlet.java
+++ src/main/java/la/servlet/RegServlet.java
@@ -1,7 +1,6 @@
 package la.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,18 +9,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/MenuServlet")
-public class MenuServlet extends HttpServlet {
-   
+@WebServlet("/RegServlet")
+public class RegServlet extends HttpServlet {
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html;charset=UTF-8");
-		PrintWriter out = response.getWriter();
-		out.println("Hello World!!");
+		
 	}
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	
 		doGet(request, response);
 	}
 
