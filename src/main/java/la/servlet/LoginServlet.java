3b76895b6f2b5275b7f82647b5be562e3c9f76d4
WIN-GG4B8FJLK5R\student <2022java42-student007@la-study.com>
2022-05-24 17:31:13 +0900

    一時保存

---
+++ src/main/java/la/servlet/LoginServlet.java
@@ -0,0 +1,42 @@
+package la.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import la.dao.LoginDAO;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Boolean b = false;
+		request.setAttribute("login", b);
+		gotoPage(request, response, "/mem_Login.jsp");
+		return;
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		int login_id = Integer.parseInt(request.getParameter("login_id"));
+		String password = request.getParameter("password");
+		LoginDAO lo = new LoginDAO();
+		int log = Integer.parseInt(lo.e(login_id, password));
+
+	}
+
+	private void gotoPage(HttpServletRequest request, HttpServletResponse response, String page)
+			throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher(page);
+		rd.forward(request, response);
+
+	}
+
+}
